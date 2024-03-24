602394aea29c265dc7bfad3bebc55734c466c908
Sagar Dhami <sagardhami2001@gmail.com>
2024-03-24 22:55:21 +0545

    fix: minor fixes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,26 +3,26 @@ import { colors, gradients } from "./src/Themes/variables";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        eerieBlack: colors.eerieBlack,
-        jet: colors.jet,
-        platinum: colors.platinum,
-        timberwolf: colors.timberwolf,
-        davysGray: colors.davysGray,
-      },
-      backgroundImage: {
-        "gradient-top": gradients.top,
-        "gradient-right": gradients.right,
-        "gradient-bottom": gradients.bottom,
-        "gradient-left": gradients.left,
-        "gradient-top-right": gradients.topRight,
-        "gradient-bottom-right": gradients.bottomRight,
-        "gradient-top-left": gradients.topLeft,
-        "gradient-bottom-left": gradients.bottomLeft,
-        "gradient-radial": gradients.radial,
-      },
+    colors: {
+      eerieBlack: colors.eerieBlack,
+      jet: colors.jet,
+      platinum: colors.platinum,
+      timberwolf: colors.timberwolf,
+      davysGray: colors.davysGray,
+      white: colors.white,
     },
+    backgroundImage: {
+      "gradient-top": gradients.top,
+      "gradient-right": gradients.right,
+      "gradient-bottom": gradients.bottom,
+      "gradient-left": gradients.left,
+      "gradient-top-right": gradients.topRight,
+      "gradient-bottom-right": gradients.bottomRight,
+      "gradient-top-left": gradients.topLeft,
+      "gradient-bottom-left": gradients.bottomLeft,
+      "gradient-radial": gradients.radial,
+    },
+    extend: {},
   },
   plugins: [],
 };
