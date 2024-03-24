35343fe25f74c0abfe46b7171a9619793751fcaa
Sagar Dhami <sagardhami2001@gmail.com>
2024-03-24 22:53:38 +0545

    fix: adde new navigation bar

--- client/src/Components/Navbar/index.tsx
+++ client/src/Components/Navbar/index.tsx
@@ -1,51 +1,42 @@
+import { MainContainer } from "../../Container/MainContainer";
+import { Link } from "react-router-dom";
+import { ReactComponent as CartIcon } from "../../Assets/icons/cart.svg";
+
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div>
-        <img src="/images/logo.png" alt="logo" width={200} />
-      </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Docs
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Examples
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-          >
-            Blog
-          </a>
+    <nav className="bg-white shadow">
+      <MainContainer>
+        <div className="flex items-center h-[70px] justify-between w-full">
+          <div className="relative top-[6px]">
+            <img src="/logo-no-background.png" alt="logo" width={110} />
+          </div>
+          <ul className="cursor-pointer text-sm text-eerieBlack">
+            <Link to="/">
+              <li className="cursor-pointer block lg:inline-block hover:text-jet mr-4">
+                Home
+              </li>
+            </Link>
+            <Link to="/shop">
+              <li className="cursor-pointer block lg:inline-block hover:text-jet mr-4">
+                Shop
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="cursor-pointer block lg:inline-block hover:text-jet mr-4">
+                About
+              </li>
+            </Link>
+            <Link to="Contact">
+              <li className="cursor-pointer block lg:inline-block hover:text-jet">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div>
+            <CartIcon />
+          </div>
         </div>
-        <div>
-          <a
-            href="#"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          >
-            Download
-          </a>
-        </div>
-      </div>
+      </MainContainer>
     </nav>
   );
 };
